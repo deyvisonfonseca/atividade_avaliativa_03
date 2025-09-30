3f4c502f6a32964c46bb2d7d0eed19b3294d0bc4
deyvisonfonseca <153868022+deyvisonfonseca@users.noreply.github.com>
2025-09-29 21:04:30 -0300

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     MONGO_URL: str
+    MONGO_DB: str = "chatdb"
     APP_HOST: str = "127.0.0.1"
     APP_PORT: int = 8000
 
---
+++ app/routes/__init__.py
@@ -0,0 +1,1 @@
+# Este arquivo faz com que o Python trate o diretório 'routes' como um pacote.
